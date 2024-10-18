15b84f1213bd4e5594ddba42ab63d42f323dc0af
Lucas Sato <lucasjks@gmail.com>
2024-10-18 16:04:45 -0700

    Parse only the state if loading state-settings

--- main.py
+++ main.py
@@ -127,10 +127,11 @@ async def main(*args):
         settings = Settings(**json.loads(f.read()))
 
     if os.environ.get("STARTING_STATE"):
-        state = State.parse_raw(os.environ["STARTING_STATE"])
+        state = State.parse_obj(json.loads(os.environ["STARTING_STATE"])["state"])
         await replay_history(state, settings)
     elif os.environ.get("STARTING_STATE_PATH"):
-        state = State.parse_file(os.environ["STARTING_STATE_PATH"])
+        with open(os.environ["STARTING_STATE_PATH"]) as f:
+            state = State.parse_obj(json.load(f)["state"])
         await replay_history(state, settings)
 
     agent = Agent(
