cebb9c848d489abbbf4a2bb4b597340b12b9d302
Brian Goodrich <brian@metr.org>
2025-01-02 19:41:08 -0800

    add fn-calling generators for 2 additional gpt-4s

--- modules/generators.py
+++ modules/generators.py
@@ -196,6 +196,8 @@ gpt_models = [
     ("gpt-4o-mini-2024-07-18", "4om"),
     ("o1-preview-2024-09-12", "o1p"),
     ("o1-mini-2024-09-12", "o1m"),
+    ("gpt-4-1106-preview", "4-1106"),
+    ("gpt-4-0125-preview", "4-0125"),
 ]
 for model_pair, n in product(gpt_models, [1, 2, 4, 8, 16, 32, 64]):
     model, desc = model_pair
